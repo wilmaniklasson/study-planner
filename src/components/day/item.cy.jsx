1cc44525aade2a6b52e6a9ba67eb28d82a3cd1a3
wilmaniklasson <wilma.niklasson2@hotmail.se>
2024-05-22 11:51:23 +0200

    -

--- src/components/day/item.cy.jsx
+++ src/components/day/item.cy.jsx
@@ -1,34 +1,27 @@
-import React from 'react'
-import Item from './Item'
-
+import React from 'react';
+import Item from './Item';
 
 describe('Item component', () => {
-  it('renders', () => {
   const todoItem = { id: 1, day: 'mo', done: true, late: false, text: 'Göra klart inlämning' };
-  cy.mount(<Item item={todoItem} />);
+
+  it('renders', () => {
+    cy.mount(<Item item={todoItem} />);
   });
 
   it('can toggle todo', () => {
-    const todoItem = { id: 1, day: 'mo', done: true, late: false, text: 'Göra klart inlämning' };
-    
     cy.mount(<Item item={todoItem} />);
     cy.get('input[type="checkbox"]').click();
   });
 
-  it ('can edit todo', () => {
-    const todoItem = { id: 1, day: 'mo', done: true, late: false, text: 'Göra klart inlämning' };
+  it('can edit todo', () => {
     cy.mount(<Item item={todoItem} />);
     cy.get('span').contains('✏️').click();
     cy.get('input[type="text"]').type('Göra klart inlämning');
     cy.get('button').contains('Spara').click();
   });
 
-  it ('can remove todo', () => {
-    const todoItem = { id: 1, day: 'mo', done: true, late: false, text: 'Göra klart inlämning' };
+  it('can remove todo', () => {
     cy.mount(<Item item={todoItem} />);
     cy.get('span').contains('🗑️').click();
   });
-
-  
-
 });
