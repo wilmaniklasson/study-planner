5cfd69a75365bf5abeb22be7ebfaefbee3c9fcf2
wilmaniklasson <wilma.niklasson2@hotmail.se>
2024-05-22 15:45:57 +0200

    -

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,30 @@
-const Header = () => (
-	<header>
-		<h1> Min vecka </h1>
-		<button className="restart-week"> Starta om vecka </button>
-	</header>
-)
+import React from 'react';
+import { useStore } from '../data/store';
 
-export default Header
+const Header = () => {
+    const todos = useStore(state => state.todos);
+    const toggleTodo = useStore(state => state.toggleTodo);
+    const totalTodos = todos.length;
+    const completedTodos = todos.filter(todo => todo.done).length;
+
+    const restartWeek = () => {
+        todos.forEach(todo => {
+            if (todo.done) {
+                toggleTodo(todo.id);
+            }
+        });
+
+    };
+
+    return (
+        <>
+            <header>
+                <h1>Min vecka</h1>
+				<p className='completedTodos'>{completedTodos}/{totalTodos} Uppgifter klara</p>
+                <button onClick={restartWeek} className="restart-week">Starta om vecka</button>
+            </header>
+        </>
+    );
+};
+
+export default Header;
